1588fb4def7baff939ecce24130214ba3829f1b9
haidynArnett <harnett7@gatech.edu>
2022-08-24 02:55:54 -0400

    added and organized logos on navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MyNavbar from './Navbar.js';
 import Headshot from './Headshot.js';
+import Minesweeper from 'minesweeper/src/Minesweeper';
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
       <Headshot />
+      <Minesweeper />
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,10 +4,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import resume from './Resume-HaidynArnett-2022.pdf';
-import Headshot from './Headshot.js'
-import reactlogo from './icons/logo.svg';
-import htmllogo from './icons/HTML5_Logo.svg';
-import csslogo from './icons/icons8-css3.svg';
+import Headshot from './Headshot.js';
+import reactlogo from './icons/reactlogo.svg';
+import htmllogo from './icons/htmllogo.svg';
+import csslogo from './icons/csslogo.svg';
 import jslogo from './icons/jslogo.png';
 
 function MyNavbar() {
@@ -16,10 +16,13 @@ function MyNavbar() {
             <Container>
                 <Navbar.Brand>Haidyn Arnett</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href='#'> Projects</Nav.Link>
+                    <NavDropdown title="Projects" id="navbarScrollingDropdown">
+                        <NavDropdown.Item onClick={console.log('hi')}>Minesweeper</NavDropdown.Item>
+                    </NavDropdown>
                     <Nav.Link href={resume}>Resume</Nav.Link>
                     <Nav.Link href='mailto:harnett7@gatech.edu'>Email</Nav.Link>
                     <Nav.Link href='https://www.linkedin.com/in/haidynArnett/'>LinkedIn</Nav.Link>
+                    <Nav.Link href='https://github.com/haidynArnett'>GitHub</Nav.Link>
                 </Nav>
                 <Nav>
                     <Navbar.Brand>
@@ -27,25 +30,25 @@ function MyNavbar() {
                             src={reactlogo}
                             width="45"
                             height="45"
-                            className="d-inline-block align-top"
+                            className="d-inline-block align-center"
                         />
                         <img
                             src={htmllogo}
-                            width="37"
-                            height="37"
-                            className="d-inline-block align-top"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-center"
                         />
                         <img
                             src={csslogo}
                             width="40"
                             height="40"
-                            className="d-inline-block align-top"
+                            className="d-inline-block align-center"
                         />
                         <img
                             src={jslogo}
-                            width="40"
-                            height="40"
-                            className="d-inline-block align-top"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-center"
                         />
                     </Navbar.Brand>
                 </Nav>
